c575ff5ed3b5c3cd4ae5d24a9051756ab0c85eb9
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-10-13 18:58:47 -0300

    Reescrevendo os códigos para que o df seja realmente limpo

--- analise_Ana.py
+++ analise_Ana.py
@@ -1,19 +1,28 @@
-import limpa_dados as ld
+from limpa_dados import remove_colunas_sem_dado, trata_celulas_vazias, corrige_nomes_df
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # definindo o df com as alterações do limpa_dados
-df = ld.df
+def define_df_limpo():
+    csv_file = "IGC_2021.csv"
+    df = pd.read_csv(csv_file)
+    df = remove_colunas_sem_dado(df)
+    df = trata_celulas_vazias(df)
+    df = corrige_nomes_df(df)
+    return df
+
+print(define_df_limpo())
 
 # Deixando como padrão letras minúsculas na coluna "Organização Acadêmica*"
 # Isso porque nela há "FACULDADE" e "faculdade" e eu preciso que o python entenda ambos como iguais. 
 
-df["Organização Acadêmica*"] = df["Organização Acadêmica*"].str.lower()
+# df["Organização Acadêmica*"] = df["Organização Acadêmica*"].str.lower()
 
 # Criando uma lista com os valores únicos da coluna "Organização Acadêmica"
-valores_unicos_org_acad = df["Organização Acadêmica*"].unique().tolist()
+# valores_unicos_org_acad = df["Organização Acadêmica"].unique().tolist()
 # print(valores_unicos_org_acad)
 
+'''
 # Criando uma lista com os valores únicos da coluna "IGC (Faixa)"
 valores_unicos_IGC_faixa = df["IGC (Faixa)"].unique().tolist()
 # print(valores_unicos_IGC_faixa)
@@ -23,6 +32,7 @@ for org_acad in valores_unicos_org_acad:
     media_IGC_faixa = df[df["Organização Acadêmica*"] == org_acad]["IGC (Faixa)"].mean()
     chave_valor = {org_acad: media_IGC_faixa}
     print(chave_valor)
+    '''
     
 
 
