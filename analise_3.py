a636142c3da28405dabf06bd7888d8890e6c1332
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-10-13 01:30:14 -0300

    Adicionado Medidas para Conceito de Mestrado
    Atualizado analise_3.py

--- analise_3.py
+++ analise_3.py
@@ -11,6 +11,23 @@ print(sigla_uf, end="\n")
 ocorrencia_por_uf = sigla_uf.value_counts().to_frame()
 print(ocorrencia_por_uf, end="\n")
 
-# Análise "Beta"
-df_analise_beta = df[["Sigla da UF", "Beta (Proporção de Mestrado - Equivalente)"]]
-print(df_analise_beta)
+# Análise "Conceito Médio de Mestrado"
+df_mestrado = df[["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"]].astype(float)
+df_mestrado = df_mestrado[(df_mestrado["Beta (Proporção de Mestrado - Equivalente)"] != 0) & (df_mestrado["Beta (Proporção de Mestrado - Equivalente)"].notna())]
+print(df_mestrado.head(10))
+
+# Medidas de tendência central e dispersão
+mestrado_conceito_medio_media = df_mestrado["Conceito Médio de Mestrado"].mean()
+print(mestrado_conceito_medio_media)
+
+mestrado_conceito_medio_mediana = df_mestrado["Conceito Médio de Mestrado"].median()
+print(mestrado_conceito_medio_mediana)
+
+mestrado_conceito_medio_std = df_mestrado["Conceito Médio de Mestrado"].std()
+print(mestrado_conceito_medio_std)
+
+mestrado_conceito_medio_max = df_mestrado["Conceito Médio de Mestrado"].max()
+print(mestrado_conceito_medio_max) 
+
+mestrado_conceito_medio_min = df_mestrado["Conceito Médio de Mestrado"].min()
+print(mestrado_conceito_medio_min)
