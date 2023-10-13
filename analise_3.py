a2cf6d2fe6366c8da91dbed3682a4debac31951d
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-10-13 20:59:30 -0300

    Algumas melhorias
    Adicionados alguns detalhes

--- analise_3.py
+++ analise_3.py
@@ -14,20 +14,20 @@ print(ocorrencia_por_uf, end="\n")
 # Análise "Conceito Médio de Mestrado"
 df_mestrado = df[["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"]].astype(float)
 df_mestrado = df_mestrado[(df_mestrado["Beta (Proporção de Mestrado - Equivalente)"] != 0) & (df_mestrado["Beta (Proporção de Mestrado - Equivalente)"].notna())]
-print(df_mestrado.head(10))
+print(df_mestrado.head(10), end="\n")
 
 # Medidas de tendência central e dispersão
 mestrado_conceito_medio_media = df_mestrado["Conceito Médio de Mestrado"].mean()
-print(mestrado_conceito_medio_media)
+print("Média de Conceito Médio de Mestrado: ", mestrado_conceito_medio_media, end="\n")
 
 mestrado_conceito_medio_mediana = df_mestrado["Conceito Médio de Mestrado"].median()
-print(mestrado_conceito_medio_mediana)
+print("Mediana de Conceito Médio de Mestrado: ", mestrado_conceito_medio_mediana, end="\n")
 
 mestrado_conceito_medio_std = df_mestrado["Conceito Médio de Mestrado"].std()
-print(mestrado_conceito_medio_std)
+print("Desvio Padrão de Conceito Médio de Mestrada: ", mestrado_conceito_medio_std, end="\n")
 
 mestrado_conceito_medio_max = df_mestrado["Conceito Médio de Mestrado"].max()
-print(mestrado_conceito_medio_max) 
+print("Conceito Médio de Mestrado Máximo: ", mestrado_conceito_medio_max, end="\n") 
 
 mestrado_conceito_medio_min = df_mestrado["Conceito Médio de Mestrado"].min()
-print(mestrado_conceito_medio_min)
+print("Conceito Médio de Mestrado Mínimo: ", mestrado_conceito_medio_min, end="\n")
