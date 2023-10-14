37b62bf001e6d1506e8314992861a153d042a2d3
GuilhermeCastilho02 <guilherme222castilho@gmail.com>
2023-10-14 19:20:53 -0300

    Fiz uma função de análise

--- analise_gmc.py
+++ analise_gmc.py
@@ -2,36 +2,51 @@ import pandas as pd
 import limpa_dados as lp
 import func_analises as fan
 
-df1 = fan.media_tres_por_indice(fan.df, ["Conceito Médio de Graduação", "Conceito Médio de Mestrado", "Conceito Médio do doutorado"], "Categoria Administrativa")
+df1 = fan.df[["Conceito Médio de Graduação", "Conceito Médio de Mestrado", "Conceito Médio do doutorado", "Categoria Administrativa"]]
+df1 = df1[df1["Conceito Médio de Graduação"] > 0]
+df1 = df1[df1["Conceito Médio de Mestrado"] > 0]
+df1 = df1[df1["Conceito Médio do doutorado"] > 0]
 
-def prints_da_analise(df1):
-    print("####################### ANÁLISE DOS CONCEITOS #######################", end="\n\n")
+df1 = df1.set_index("Categoria Administrativa")
+
+def prints_da_analise_geral(df1):
+    pass
+
+
+df2 = fan.media_tres_por_indice(fan.df, ["Conceito Médio de Graduação", "Conceito Médio de Mestrado", "Conceito Médio do doutorado"], "Categoria Administrativa")
+
+def prints_da_analise_das_medias(df2):
+    print("####################### ANÁLISE DAS MÉDIAS DOS CONCEITOS MÉDIOS#######################", end="\n\n")
     # Vemos no df abaixo as médias dos conceitos médios em cada nível do ensino superior por categoria administrativa
-    print(df1, end="\n\n")
+    print(df2, end="\n\n")
 
     # Vemos a partir da tabela abaixo as categorias administrativas com os maiores Conceitos médios em cada nível de ensino superior
-    print(df1[df1 == df1.max()], end="\n\n")
+    print(df2[df2 == df2.max()], end="\n\n")
     # A categoria administrativa "Especial" possui a maior média tanto no conceito médio de graduação(3.059135) quanto no do doutorado(4.66137)
     # "Pública Federal" possui a maior média no conceito médio de mestrado(4.3846)
 
     # Vemos a partir da tabela abaixo as categorias administrativas com os menores Conceitos médios em cada nível de ensino superior
-    print(df1[df1 == df1.min()], end="\n\n")
+    print(df2[df2 == df2.min()], end="\n\n")
     # A categoria administrativa "Pública Municipal" possui a menor média do conceito médio do doutorado(4.579625)
     # A categoria administrativa "Especial" possui a menor média do conceito médio de mestrado(4)
     # "Pública Federal" possui a menor média no conceito médio de graduação(2.226348)
 
 
     # O print abaixo nos revela as médias das 3 colunas
-    print(df1.mean(), end="\n\n")
-    # Vemos que a maior média é do doutorado e a menor é de graduação
+    print("AS médias de cada nível de ensino são: ", df2.mean(), sep="\n", end="\n\n")
+    # Vemos que a maior média é do doutorado(4.628627) e a menor é de graduação(2.573522)
 
-prints_da_analise(df1)
+    # O print abaixo nos revela as medianas das 3 colunas
+    print("AS medianas de cada nível de ensino são: ", df2.median(), sep="\n", end="\n\n")
+    # O maior valor novamente é do doutorado(4.64235) e o menor e da graduação(2.525514)
 
-df2 = fan.df[["Conceito Médio de Graduação", "Conceito Médio de Mestrado", "Conceito Médio do doutorado", "Categoria Administrativa"]]
-df2 = df2[df2["Conceito Médio de Graduação"] > 0]
-df2 = df2[df2["Conceito Médio de Mestrado"] > 0]
-df2 = df2[df2["Conceito Médio do doutorado"] > 0]
+    # A partir das medidas de resumo acima, vemos que a mediana e a média são muito proximas em seus respectivos níveis de graduação
+    # Vemos também que o mestrado se mantém bem próximo do doutorado e distante da graduação nas notas (sua média é 4.267921 e sua mediana é 4.304046)
 
-df2 = df2.set_index("Categoria Administrativa")
-#         # # Garantindo que o df esteja tratado
-#         # df = lp.corrige_nomes_df(df)
+    # O print abaixo dos da o desvioo padrão das médias
+    print("O devio padrão em cada nível de ensino são: ", df2.std(), sep="\n", end="\n\n")
+    # O maior desvio é da graduação(0.293501) e o menor o do doutorado(0.036502). O desvio padrão das médias do mestrado é 0.137659
+
+
+
+
