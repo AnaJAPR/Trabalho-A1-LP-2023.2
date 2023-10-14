6036b44b352e8deb196b768109f3d8beb45b3295
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-10-13 21:26:18 -0300

    Desenvolvimento de função
    Criando uma função de seleção.

--- analise_3.py
+++ analise_3.py
@@ -4,6 +4,38 @@ import matplotlib.pyplot as plt
 
 df = lp.corrige_nomes_df(lp.df)
 
+def selecionar_colunas_eliminando_nulos_gmd(df:pd.core.frame.DataFrame, colunas:list):
+    """
+    Parameters
+    ----------
+    df : pandas.core.frame.DataFrame
+        Recebe um DataFrame.
+    colunas : list
+        Recebe uma lista de colunas pertencentes ao DataFrame.
+
+        DESCRIPTION. A função tem como objetivo selecionar um determinado conjunto de
+        colunas de um DataFrame e remover todas as linhas tais que a primeira coluna
+        possua valor igual a 0 ou nulo.
+        
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        Retorna um DataFrame contendo as 3 colunas passadas
+
+    """
+    try:
+        if type(df) != pd.core.frame.DataFrame:
+            raise TypeError("O argumento passado não é um DataFrame Pandas.")
+        if type(colunas) != list:
+            raise TypeError("O argumento passado não é uma lista.")
+    except TypeError:
+        print("Um ou mais argumentos passados não corresponde ao tipo solicitado.")
+    else:
+        new_df = df[[colunas[0], colunas[1]]].astype(float)
+        new_df = new_df[(new_df[colunas[0]] != 0) & (new_df[colunas[0]].notna())]
+    finally:
+        return new_df
+
 # Análise "Instituições de Ensino por UF"
 sigla_uf = df["Sigla da UF"]
 print(sigla_uf, end="\n")
@@ -12,8 +44,7 @@ ocorrencia_por_uf = sigla_uf.value_counts().to_frame()
 print(ocorrencia_por_uf, end="\n")
 
 # Análise "Conceito Médio de Mestrado"
-df_mestrado = df[["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"]].astype(float)
-df_mestrado = df_mestrado[(df_mestrado["Beta (Proporção de Mestrado - Equivalente)"] != 0) & (df_mestrado["Beta (Proporção de Mestrado - Equivalente)"].notna())]
+df_mestrado = selecionar_colunas_eliminando_nulos_gmd(df, ["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"])
 print(df_mestrado.head(10), end="\n")
 
 # Medidas de tendência central e dispersão
