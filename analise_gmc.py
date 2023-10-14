fa030210360c0538ec150bb1c611074c6eb2b445
GuilhermeCastilho02 <guilherme222castilho@gmail.com>
2023-10-14 14:30:03 -0300

    Otimizei e transferi de módulo a função media_tres_por_indice

--- analise_gmc.py
+++ analise_gmc.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import limpa_dados as lp
+import func_analises as fan
 
-df1 = lp.media_tres_por_indice(lp.df, ["Conceito Médio de Graduação", "Conceito Médio de Mestrado", "Conceito Médio do doutorado"], "Categoria Administrativa")
+df1 = fan.media_tres_por_indice(fan.df, ["Conceito Médio de Graduação", "Conceito Médio de Mestrado", "Conceito Médio do doutorado"], "Categoria Administrativa")
 
 # Vemos a partir da tabela abaixo as categorias administrativas com os maiores Conceitos médios em cada nível de ensino superior
 print(df1[df1 == df1.max()])
@@ -9,3 +10,5 @@ print(df1[df1 == df1.max()])
 # "Pública Federal" possui a maior média no conceito médio de mestrado(4.3846)
 print(df1.mean())
 
+        # # Garantindo que o df esteja tratado
+        # df = lp.corrige_nomes_df(df)
--- func_analises.py
+++ func_analises.py
@@ -90,6 +90,89 @@ def medidas_tendencia_e_dispersao(df:pd.core.frame.DataFrame, coluna:str):
               "Mínimo": minimo}
     return resumo
 
+def media_tres_por_indice(df:pd.core.frame.DataFrame, lista_colunas:list, indice:str):
+    """
+    Parameters
+    ----------
+    df : pandas.core.frame.DataFrame
+        Recebe um DataFrame tratado
+    lista_colunas : list
+        Recebe uma lista com exatamente os nomes de 3 colunas nunéricas do DataFrame
+    filtro : str
+        Uma string de um nome de uma coluna não-numérica
+
+        DESCRIPTION. A função recebe um DataFrame tratado e retorna um DataFrame com a coluna escolhida em "indice"
+        como indice, colunas com a media dos dados não nulos das escolhidas em "lista_colunas" por cada registro do "indice".
+        
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        Retorna um DataFrame com index=indice, colunas=lista_colunas e os dados vão ser as 
+        médias dos dados não nulos das colunas escolhidas do DataFrame original.
+    """
+    try:
+        # Testando se foi passado corretamente um DataFrame como parâmetro
+        if not type(df) == pd.core.frame.DataFrame or not type(lista_colunas) == list or not type(indice) == str:
+            raise TypeError
+        # Testando se lista_colunas foi passado com o n° correto de elementos
+        if not len(lista_colunas) == 3:
+            raise ValueError("A lista deve conter 3 elementos!")
+        
+        for elemento in lista_colunas:
+            if elemento not in df.columns:
+                raise ValueError(f"O nome da coluna foi escrito errado em '{elemento}'!")
+        
+        if indice not in df.columns:
+            raise ValueError("O nome escolhido para indice não é o nome de uma coluna do DataFrame!")
+        
+    except TypeError:
+        print("TypeError: A função tem que receber um Dataframe, uma lista e uma string!")
+
+    else:
+        df_filtrado = df[[indice, lista_colunas[0], lista_colunas[1], lista_colunas[2]]]
+
+        # Retirando os 0, pois não serão úteis para esta análise
+        df_filtrado1 = df_filtrado[df_filtrado[lista_colunas[0]] > 0]
+        df_filtrado2 = df_filtrado[df_filtrado[lista_colunas[1]] > 0]
+        df_filtrado3 = df_filtrado[df_filtrado[lista_colunas[2]] > 0]
+
+        lista_registros = df_filtrado[indice].unique().tolist()
+        
+        dic_medias = dict()
+        for registro in lista_registros:
+            
+            media_registro_1 = df_filtrado1[df_filtrado1[indice] == registro][lista_colunas[0]].mean()
+            media_registro_2 = df_filtrado2[df_filtrado2[indice] == registro][lista_colunas[1]].mean()
+            media_registro_3 = df_filtrado3[df_filtrado3[indice] == registro][lista_colunas[2]].mean()
+            medias = [media_registro_1, media_registro_2, media_registro_3]
+            dic_medias[registro] = medias
+
+        colunas = ["Média " + lista_colunas[0], "Média " + lista_colunas[1], "Média " + lista_colunas[2]]
+        dados = list()
+        
+        for valor in dic_medias.values():
+            dados.append(valor)
+
+        df_medias = pd.DataFrame(dados, index=sorted(dic_medias.keys()), columns=colunas)
+
+        # Caso aconteça algum erro de aparecer valores NaN no df final:
+        for coluna in df_medias:
+            for index in lista_registros:#Pegando os indices do df_medias
+                    
+                    # Substituindo os NaN por "-" para facillitar o tratamento
+                    df_medias.fillna("-", inplace=True)
+                    if df_medias[coluna].loc[index] == "-":# Localizando onde estaria o NaN
+
+                        if coluna == "Média " + lista_colunas[0] or coluna == "Média " + lista_colunas[1] or coluna == "Média " + lista_colunas[2]:
+                            nome_coluna = coluna[6:]# Pegando o nome da coluna no df original
+
+                        # Executando novamente o que ocorre no for que calcula as médias
+                        df_medias[coluna].loc[index] = df_filtrado3[df_filtrado3[indice] == index][nome_coluna].mean()
+
+        return df_medias
+
 if __name__ == "__main__":
     doctest.testfile("doctest_folder\doctest-selecionar_colunas_eliminando_nulos.txt", verbose=True)
     doctest.testfile("doctest_folder\doctest-medidas_tendencia_e_dispersao.txt", verbose=True)
+    doctest.testfile("doctest_folder\doctest-media_tres_por_indice.txt", verbose=True)
+    
