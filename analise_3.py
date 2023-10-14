161f5ec958e10f4c27129fc7d716c4ddf240d795
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-10-14 01:48:53 -0300

    Criada função de medidas resumo

--- analise_3.py
+++ analise_3.py
@@ -44,32 +44,66 @@ def selecionar_colunas_eliminando_nulos(df:pd.core.frame.DataFrame, colunas:list
     finally:
         return new_df
 
-# # Análise "Instituições de Ensino por UF"
-# sigla_uf = df["Sigla da UF"]
-# print(sigla_uf, end="\n")
+def medidas_tendencia_e_dispersão(df, coluna):
+    """
+    Parameters
+    ----------
+    df : pandas.core.frame.DataFrame
+        Recebe um DataFrame.
+    coluna : str
+        Recebe uma string que representa o nome de uma coluna do DataFrame.
 
-# ocorrencia_por_uf = sigla_uf.value_counts().to_frame()
-# print(ocorrencia_por_uf, end="\n")
+        DESCRIPTION. A função analisa uma coluna do DataFrame e extrai as informações
+        de média, mediana, desvio padrão, máximo e mínimo.
+        
+    Returns
+    -------
+    dict
+        Retorna um dicionário contendo a medida resumo ou de dispersão como chave e o
+        seu valor como valor.
 
-# # Análise "Conceito Médio de Mestrado"
-# df_mestrado = selecionar_colunas_eliminando_nulos(df, ["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"])
-# print(df_mestrado, end="\n")
+    """
+    try:
+        if type(df) != pd.core.frame.DataFrame:
+            raise TypeError("O argumento passado não é um DataFrame Pandas.")
+        if type(coluna) != str:
+            raise TypeError("O argumento passado não é uma string.")
+        if not coluna in df.columns:
+            raise NameError("A coluna passada não está presente no DataFrame")
+        if not pd.api.types.is_numeric_dtype(df[coluna]):
+            raise ValueError("A coluna passada não é numérica")
+    except TypeError as erro:
+        return str(erro)
+    except NameError as erro:
+        return str(erro)
+    except ValueError as erro:
+        return str(erro)
+    else:
+        media = df[coluna].mean()
+        mediana = df[coluna].median()
+        desvio_padrao = df[coluna].std()
+        maximo = df[coluna].max()
+        minimo = df[coluna].min()
+    resumo = {"Média":media,
+              "Mediana":mediana,
+              "Desvio Padrão": desvio_padrao,
+              "Máximo": maximo,
+              "Mínimo": minimo}
+    return resumo
+
+# Análise "Instituições de Ensino por UF"
+sigla_uf = df["Sigla da UF"]
+print(sigla_uf, end="\n")
+
+ocorrencia_por_uf = sigla_uf.value_counts().to_frame()
+print(ocorrencia_por_uf, end="\n")
+
+# Análise "Conceito Médio de Mestrado"
+df_mestrado = selecionar_colunas_eliminando_nulos(df, ["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"])
+print(df_mestrado, end="\n")
 
 # Medidas de tendência central e dispersão
-# mestrado_conceito_medio_media = df_mestrado["Conceito Médio de Mestrado"].mean()
-# print("Média de Conceito Médio de Mestrado: ", mestrado_conceito_medio_media, end="\n")
-
-# mestrado_conceito_medio_mediana = df_mestrado["Conceito Médio de Mestrado"].median()
-# print("Mediana de Conceito Médio de Mestrado: ", mestrado_conceito_medio_mediana, end="\n")
-
-# mestrado_conceito_medio_std = df_mestrado["Conceito Médio de Mestrado"].std()
-# print("Desvio Padrão de Conceito Médio de Mestrada: ", mestrado_conceito_medio_std, end="\n")
-
-# mestrado_conceito_medio_max = df_mestrado["Conceito Médio de Mestrado"].max()
-# print("Conceito Médio de Mestrado Máximo: ", mestrado_conceito_medio_max, end="\n") 
-
-# mestrado_conceito_medio_min = df_mestrado["Conceito Médio de Mestrado"].min()
-# print("Conceito Médio de Mestrado Mínimo: ", mestrado_conceito_medio_min, end="\n")
+print(medidas_tendencia_e_dispersão(df_mestrado, "Conceito Médio de Mestrado"))
 
 if __name__ == "__main__":
     doctest.testfile("doctest_folder\doctest-selecionar_colunas_eliminando_nulos.txt", verbose=True)
