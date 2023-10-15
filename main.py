4a72a0d8ab52410568e4ee3dbaddd7d8105fc840
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-10-15 08:07:05 -0300

    adiciona linhas que chamam as minhas funções de análise

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import limpa_dados as lpimport pandas as pd#import analise_Otavio as aodf = pd.read_csv("IGC_2021.csv")df = lp.corrige_nomes_df(lp.df)#ao.analise_org_media_num_cursos(df)#ao.analise_org_num_catadm_empilhado(df)#ao.analise_intervalos_igc_catadm_pizza(df)
+import limpa_dados as lp
+import pandas as pd
+#import analise_Otavio as ao
+# import analise_Ana as aa
+
+df = pd.read_csv("IGC_2021.csv")
+
+df = lp.corrige_nomes_df(lp.df)
+
+#ao.analise_org_media_num_cursos(df)
+#ao.analise_org_num_catadm_empilhado(df)
+#ao.analise_intervalos_igc_catadm_pizza(df)
+
+# aa.cria_plot_1_ana(aa.analise_1_ana(df))
+# aa.cria_plot_2_ana(aa.analise_ana_2(aa.analise_1_ana(df)))
