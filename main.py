2a4360b147a8618a8e90b00a47976652b3fe3e43
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-10-15 10:07:09 -0300

    Tentativa de plotar gráficos em um único html

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import limpa_dados as lp
 import pandas as pd
-#import analise_Otavio as ao
-# import analise_Ana as aa
+from dominate import document
+from dominate.tags import h1, img
+import analise_Ana as aa
+# import analise_Otavio as ao
 
 df = pd.read_csv("IGC_2021.csv")
-
 df = lp.corrige_nomes_df(lp.df)
+
+# Cria uma página HTML
+doc = document(title="Meus Gráficos")
+
+# Adiciona títulos e gráficos à página
+with doc:
+    h1("Gráfico 1")
+    img(src=aa.cria_plot_1_ana(aa.analise_1_ana(df)), width="600", height="400")
+    
+    h1("Gráfico 2")
+    img(src=aa.cria_plot_2_ana(aa.analise_ana_2(aa.analise_1_ana(df))), width="600", height="400")
+
+# Salva a página HTML em um arquivo
+with open("index.html", "w") as f:
+    f.write(doc.render())
 
 #ao.analise_org_media_num_cursos(df)
 #ao.analise_org_num_catadm_empilhado(df)
