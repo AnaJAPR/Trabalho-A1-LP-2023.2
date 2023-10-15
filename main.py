5aa1db478f4ccf5a19695750f9a2a43566b8eee0
Otavio Bettega <otaviofbettega@gmail.com>
2023-10-15 04:23:29 -0300

    Iniciação do main para chamar as analises dos membros do grupo

---
+++ main.py
@@ -0,0 +1,1 @@
+import limpa_dados as lpimport pandas as pd#import analise_Otavio as aodf = pd.read_csv("IGC_2021.csv")df = lp.corrige_nomes_df(lp.df)#ao.analise_org_media_num_cursos(df)#ao.analise_org_num_catadm_empilhado(df)#ao.analise_intervalos_igc_catadm_pizza(df)
