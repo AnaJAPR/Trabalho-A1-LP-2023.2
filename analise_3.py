1477c139f231ee86348d83b8b5253eebc1e17792
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-10-14 21:09:48 -0300

    Remoções

--- analise_3.py
+++ analise_3.py
@@ -13,29 +13,17 @@ ocorrencia_por_uf = sigla_uf.value_counts().to_frame()
 print(ocorrencia_por_uf, end="\n")
 
 # Análise "Conceito Médio"
-# "Graduação"
-df_graduacao = fan.selecionar_colunas_eliminando_nulos(df, ["Alfa (Proporção de Graduação)", "Conceito Médio de Graduação"])
-print(df_graduacao)
-# Medidas de tendência central e dispersão
-print(fan.medidas_tendencia_e_dispersao(df_graduacao, "Conceito Médio de Graduação"))
-
 # "Mestrado"
 df_mestrado = fan.selecionar_colunas_eliminando_nulos(df, ["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"])
 print(df_mestrado, end="\n")
 # Medidas de tendência central e dispersão
 print(fan.medidas_tendencia_e_dispersao(df_mestrado, "Conceito Médio de Mestrado"))
 
-# "Doutorado"
-df_doutorado = fan.selecionar_colunas_eliminando_nulos(df, ["Gama (Proporção de Doutorandos – Equivalente)", "Conceito Médio do doutorado"])
-print(df_doutorado)
-# Medidas de tendência central e dispersão
-print(fan.medidas_tendencia_e_dispersao(df_doutorado, "Conceito Médio do doutorado"))
-
 # Desenvolvimento de Gráficos
 def grafico_1():
     bplot = plt.boxplot(df_mestrado["Conceito Médio de Mestrado"], patch_artist=True)
     for box in bplot['boxes']:
-        box.set(facecolor='blue')
+            box.set(facecolor="red")
     plt.title("BoxPlot - Conceito Médio de Mestrado")
     plt.xlabel("Mestrado")
     plt.ylabel("Conceito Médio")
