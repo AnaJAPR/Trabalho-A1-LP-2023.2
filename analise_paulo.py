bb96d60a0e14b04c2665a324aa450849a8ac7db8
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-10-16 01:07:01 -0300

    Corrigindo falhas de funções

--- analise_paulo.py
+++ analise_paulo.py
@@ -7,62 +7,154 @@ import pandas as pd
 # Desenvolvimento de Gráficos
 # CONCEITO MÉDIO DE MESTRADO
 def graf_boxplot_conceito_medio_mestrado(df):
-    # Selecionando as colunas de "Beta" e "Conceito de Mestrado"
-    df_mestrado = fan.selecionar_colunas_eliminando_nulos(df, ["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"])
-    bplot = plt.boxplot(df_mestrado["Conceito Médio de Mestrado"], patch_artist=True)
-    
-    # Personalizando o BoxPlot
-    for box in bplot['boxes']:
-        box.set(facecolor="#8B0000")
-    plt.title("BoxPlot - Conceito Médio de Mestrado")
-    plt.xlabel("Mestrado")
-    plt.ylabel("Conceito Médio")
-    plt.gca().set_xticklabels([])
+    """
+    Parameters
+    ----------
+    df: pandas.core.frame.DataFrame 
 
-    plt.savefig("graphic_folder/grafico_3.png")
-    plt.show()
+        DESCRIPTION. A função extrai as colunas "Beta (Proporção de Mestrado - Equivalente)" 
+        e "Conceito Médio de Mestrado" do DataFrame de modo a remover todas as linhas vazias.
+        Em seguida é criado um boxplot.
+
+    Returns
+    -------
+    None
+        A função somente exibe o gráfico criado e o salva em um arquivo na pasta "graphic_folder"
+    """
+    try:
+        if not type(df) == pd.core.frame.DataFrame:
+            raise TypeError("O argumento passado não é um DataFrame.")
+        if not ["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"] in df.columns:
+            raise NameError("O DataFrame necessita conter as colunas 'Beta (Proporção de Mestrado - Equivalente)' e 'Conceito Médio de Mestrado'.")
+        if not pd.api.types.is_numeric_dtype(df["Beta (Proporção de Mestrado - Equivalente)"]) or not pd.api.types.is_numeric_dtype(df["Conceito Médio de Mestrado"]):
+            raise ValueError("As colunas 'Beta (Proporção de Mestrado - Equivalente)' e 'Conceito Médio de Mestrado' necessitam ser numéricas.")
+    except TypeError as erro:
+        return str(erro)
+    except NameError as erro:
+        return str(erro)
+    except ValueError as erro:
+        return str(erro)
+    
+    else:
+        # Selecionando as colunas de "Beta" e "Conceito de Mestrado"
+        df_mestrado = fan.selecionar_colunas_eliminando_nulos(df, ["Beta (Proporção de Mestrado - Equivalente)", "Conceito Médio de Mestrado"])
+        bplot = plt.boxplot(df_mestrado["Conceito Médio de Mestrado"], patch_artist=True)
+    
+        # Personalizando o BoxPlot
+        for box in bplot['boxes']:
+            box.set(facecolor="#8B0000")
+        plt.title("BoxPlot - Conceito Médio de Mestrado")
+        plt.xlabel("Mestrado")
+        plt.ylabel("Conceito Médio")
+        plt.gca().set_xticklabels([])
+
+        plt.savefig("graphic_folder/grafico_3.png")
+        plt.show()
 
 # INSTITUIÇÕES DE ENSINO POR UF
-def graf_mapa_instituições_por_uf(df):
-    # Extraindo a coluna "Sigla da UF" e realizando value_counts()
-    sigla_uf = df["Sigla da UF"]
-    ocorrencia_por_uf = sigla_uf.value_counts().to_frame()
+def graf_mapa_instituições_por_uf(df, path_shapefile):
+    """
+    Parameters
+    ----------
+    df: pandas.core.frame.DataFrame
+    path_shapefile: str
 
-    # Utilizando um shapefile para o desenvolvimento de um gráfico de mapa
-    brasil = gpd.read_file("shapefiles\estados_2010\estados_2010.shp")
-    merge = brasil.merge(ocorrencia_por_uf, left_on="sigla", right_on="Sigla da UF", how="left")
-    merge.plot(column="count", cmap="viridis", legend=True)
+        DESCRIPTION. A função extrai a coluna "Sigla da UF" e cálculo quantas vezes cada
+        valor aparece. O shapefile dado como argumento é passado e, juntamente com a
+        informação da contagem é criado um gráfico de mapa.
+        
+    Returns
+    -------
+    None
+        A função somente exibe o gráfico criado e o salva em um arquivo na pasta "graphic_folder"
+    """
+    try:
+        if not type(df) == pd.core.frame.DataFrame:
+            raise TypeError("O argumento passado não é um DataFrame.")
+        if not ["Sigla da UF"] in df.columns:
+            raise NameError("O DataFrame necessita conter a coluna 'Sigla da UF'.")
+        if pd.api.types.is_number(df["Sigla da UF"]):
+            raise ValueError("A coluna 'Sigla da UF' não deve ser numérica.")
+    except TypeError as erro:
+        return str(erro)
+    except NameError as erro:
+        return str(erro)
+    except ValueError as erro:
+        return str(erro)
 
-    # Personalizando o gráfico
-    plt.title("Quantidade de Instituições de Ensino por Estado")
-    plt.axis("off")
+    else:
+        # Extraindo a coluna "Sigla da UF" e realizando value_counts()
+        sigla_uf = df["Sigla da UF"]
+        ocorrencia_por_uf = sigla_uf.value_counts().to_frame()
 
-    plt.savefig("graphic_folder/grafico_4.png")
-    plt.show()
+        # Utilizando um shapefile para o desenvolvimento de um gráfico de mapa
+        brasil = gpd.read_file(path_shapefile)
+        merge = brasil.merge(ocorrencia_por_uf, left_on="sigla", right_on="Sigla da UF", how="left")
+        merge.plot(column="count", cmap="viridis", legend=True)
+
+        # Personalizando o gráfico
+        plt.title("Quantidade de Instituições de Ensino por Estado")
+        plt.axis("off")
+
+        plt.savefig("graphic_folder/grafico_4.png")
+        plt.show()
 
 # ALFA, BETA E GAMA
 def graf_hist_alfa_beta_gama(df):
-    df_alfa_beta_gama = df[["Alfa (Proporção de Graduação)", "Beta (Proporção de Mestrado - Equivalente)", "Gama (Proporção de Doutorandos – Equivalente)"]]
-    df_alfa_beta_gama["Soma"] = df_alfa_beta_gama["Alfa (Proporção de Graduação)"] + df_alfa_beta_gama["Beta (Proporção de Mestrado - Equivalente)"] + df_alfa_beta_gama["Gama (Proporção de Doutorandos – Equivalente)"]
+    """
+    Parameters
+    ----------
+    df: pandas.core.frame.DataFrame
 
-    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
-    axs[0].hist(df_alfa_beta_gama["Alfa (Proporção de Graduação)"], bins=10, color="#008B00")
-    axs[0].set_title("Proporção de Graduação")
-    axs[0].set_xlim(0, 1)
-    axs[0].set_xlabel("Proporção")
-    axs[0].set_ylabel("Número de Instituições de Ensino")
+        DESCRIPTION. A função extrai as colunas de proporção de graduação, mestrado e doutorado
+        Com elas é criado um gráfico contendo um histograma para cada uma das colunas.
+        
+    Returns
+    -------
+    None
+        A função somente exibe o gráfico criado e o salva em um arquivo na pasta "graphic_folder"
+    """
+    try:
+        if not type(df) == pd.core.frame.DataFrame:
+            raise TypeError("O argumento passado não é um DataFrame.")
+        if not ["Alfa (Proporção de Graduação)","Beta (Proporção de Mestrado - Equivalente)", "Gama (Proporção de Doutorandos – Equivalente)"] in df.columns:
+            raise NameError("O DataFrame necessita conter as colunas 'Beta (Proporção de Mestrado - Equivalente)' e 'Conceito Médio de Mestrado'.")
+        if not pd.api.types.is_numeric_dtype(df["Alfa (Proporção de Graduação)"]) or not pd.api.types.is_numeric_dtype(df["Beta (Proporção de Mestrado - Equivalente"]) or not pd.api.types.is_numeric_dtype(df["Gama (Proporção de Doutorandos – Equivalente)"]):
+            raise ValueError("As colunas 'Beta (Proporção de Mestrado - Equivalente)' e 'Conceito Médio de Mestrado' necessitam ser numéricas.")
+    
+    except TypeError as erro:
+        return str(erro)
+    except NameError as erro:
+        return str(erro)
+    except ValueError as erro:
+        return str(erro)
+    else:
+        # Extraindo as colunas
+        df_alfa_beta_gama = df[["Alfa (Proporção de Graduação)", "Beta (Proporção de Mestrado - Equivalente)", "Gama (Proporção de Doutorandos – Equivalente)"]]
 
-    axs[1].hist(df_alfa_beta_gama["Beta (Proporção de Mestrado - Equivalente)"], bins=10, color="#8B0000")
-    axs[1].set_title("Proporção de Mestrado")
-    axs[1].set_xlim(0, 1)
-    axs[1].set_xlabel("Proporção")
+        # Criando cada um dos subgráficos
+        fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+        axs[0].hist(df_alfa_beta_gama["Alfa (Proporção de Graduação)"], bins=10, color="#008B00")
+        axs[1].hist(df_alfa_beta_gama["Beta (Proporção de Mestrado - Equivalente)"], bins=10, color="#8B0000")
+        axs[2].hist(df_alfa_beta_gama["Gama (Proporção de Doutorandos – Equivalente)"], bins=10, color="#00008B")
+    
+        # Personalizando o grádico de graduação
+        axs[0].set_title("Proporção de Graduação")
+        axs[0].set_xlim(0, 1)
+        axs[0].set_xlabel("Proporção")
+        axs[0].set_ylabel("Número de Instituições de Ensino")
 
-    axs[2].hist(df_alfa_beta_gama["Gama (Proporção de Doutorandos – Equivalente)"], bins=10, color="#00008B")
-    axs[2].set_title("Proporção de Doutorado")
-    axs[2].set_xlim(0, 1)
-    axs[2].set_xlabel("Proporção")
+        # Personalizando o gráfico de mestrado
+        axs[1].set_title("Proporção de Mestrado")
+        axs[1].set_xlim(0, 1)
+        axs[1].set_xlabel("Proporção")
 
-    plt.tight_layout()
+        # Personalizando o gráfico de doutorado
+        axs[2].set_title("Proporção de Doutorado")
+        axs[2].set_xlim(0, 1)
+        axs[2].set_xlabel("Proporção")
 
-    plt.savefig("graphic_folder/grafico_5.png")
-    plt.show()
+        plt.tight_layout()
+
+        plt.savefig("graphic_folder/grafico_5.png")
+        plt.show()
